fe9ad0b6479e34e8e93df4b3e358c9c8e7417da2
Kevin <kevinqiyf@hotmail.com>
2019-05-08 12:29:14 -0700

    completed basic get and post routes

--- server/routes/strings.js
+++ server/routes/strings.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const router = express.Router();
-// const db = require('../db');
+const db = require('../db');
+
+router.get('', async (req, res, next) => {
+  try {
+    const results = await db.query('SELECT * FROM strings');
+    return res.json(results.rows);
+  } catch (err) {
+    return next(err);
+  }
+});
+
+router.post('', async (req, res, next) => {
+  try {
+    const result = await db.query(
+      'INSERT INTO strings (string) VALUES ($1) RETURNING *',
+      [req.body.string],
+    );
+    return res.json(result.rows[0]);
+  } catch (err) {
+    return next(err);
+  }
+});
 
 module.exports = router;
