fdbfa280485925a93ff8d9e7d3076c30b1a89579
Kevin <kevinqiyf@hotmail.com>
2019-05-08 12:58:35 -0700

    added simple form with react hook

--- app/containers/FormPage/index.js
+++ app/containers/FormPage/index.js
@@ -4,7 +4,7 @@
  *
  */
 
-import React from 'react';
+import React, { useState } from 'react';
 // import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -20,7 +20,26 @@ export function FormPage() {
   useInjectReducer({ key: 'formPage', reducer });
   useInjectSaga({ key: 'formPage', saga });
 
-  return <div>formpage</div>;
+  const [inputString, setinputString] = useState('');
+
+  const inputChangeHandler = e => {
+    setinputString(e.target.value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setinputString('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        String:
+        <input type="text" value={inputString} onChange={inputChangeHandler} />
+      </label>
+      <input type="submit" value="Add" />
+    </form>
+  );
 }
 
 // FormPage.propTypes = {
